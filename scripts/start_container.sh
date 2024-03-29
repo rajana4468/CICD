74ab5b87b6a4db7a47323896e9ee8ba762f4608e
rajana4468 <147534727+rajana4468@users.noreply.github.com>
2024-03-29 16:56:38 +0530

    add files for deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull saikrishna1990/simple_python_flask_app:latest
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 saikrishna1990/simple_python_flask_app:latest
