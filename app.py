8dfe632af379a5d0211024021a22aabee3789598
rajana4468 <147534727+rajana4468@users.noreply.github.com>
2024-03-29 14:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello  World!, Iam Sai Krishna'
+    return 'Hello  World!, Iam Sai Krishna Rajana'
 
 if __name__ == '__main__':
     app.run()
